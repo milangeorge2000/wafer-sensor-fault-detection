0a629d01fd5d1b778c64d56525a61cd1397952b1
milangeorge2000 <45516866+milangeorge2000@users.noreply.github.com>
2021-09-13 07:12:15 -0700

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To build a classification methodology to predict the quality of wafer sensors based on the given training data',
+    author='Milan Varghese George',
+    license='MIT',
+)
